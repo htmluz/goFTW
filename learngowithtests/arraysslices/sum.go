7f7433f5baf2b2aa8d102bfc28d4e5250ab73616
Luiz Krol <luizeduardokrolpereira@gmail.com>
2024-03-21 21:03:01 -0300

    lazyyy

---
+++ learngowithtests/arraysslices/sum.go
@@ -0,0 +1,17 @@
+package arraysslices
+
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(n ...[]int) []int {
+	var sums []int
+	for _, numbers := range n {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
