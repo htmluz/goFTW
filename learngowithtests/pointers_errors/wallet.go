41c13b6c28d61d94759bf4e421fb51d3c0946226
LUIZ EDUARDO KROL PEREIRA <luiz.pereira@redeunifique.com.br>
2024-04-08 18:01:37 -0300

    pointers and errors

---
+++ learngowithtests/pointers_errors/wallet.go
@@ -0,0 +1,29 @@
+package pointers_errors
+
+import "fmt"
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Stringer interface {
+	String() string
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
