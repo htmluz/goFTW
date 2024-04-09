90905ce6c3e7c8bd437a53fb4954050e9e79edf1
LUIZ EDUARDO KROL PEREIRA <luiz.pereira@redeunifique.com.br>
2024-04-09 13:11:35 -0300

    few

--- learngowithtests/pointers_errors/wallet_test.go
+++ learngowithtests/pointers_errors/wallet_test.go
@@ -10,6 +10,15 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s want %s", got, want)
 		}
 	}
+	assertError := func(t testing.TB, got, want error) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("wanted an error but didn't get one")
+		}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -19,5 +28,14 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, startingBalance)
 	})
 }
